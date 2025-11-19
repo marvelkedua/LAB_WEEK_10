5a9ce504117297bf0304885af4b9ea75b7b16941
marvelkn <marvel_kevin12@yahoo.com>
2025-11-19 13:59:52 +0700

    Commit No. 1: Implement ViewModel to Handle Rotation

---
+++ app/src/main/java/com/example/lab_week_10/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.lab_week_10
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import com.example.lab_week_10.viewmodels.TotalViewModel
+
+class MainActivity : AppCompatActivity() {
+    // Inisialisasi ViewModel secara Lazy
+    private val viewModel by lazy {
+        ViewModelProvider(this)[TotalViewModel::class.java]
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        prepareViewModel()
+    }
+
+    private fun updateText(total: Int) {
+        findViewById<TextView>(R.id.text_total).text = getString(R.string.text_total, total)
+    }
+
+    private fun prepareViewModel() {
+        // Set nilai awal (untuk handle rotasi agar tetap muncul nilai terakhir)
+        updateText(viewModel.total)
+
+        findViewById<Button>(R.id.button_increment).setOnClickListener {
+            viewModel.incrementTotal()
+            updateText(viewModel.total)
+        }
+    }
+}
